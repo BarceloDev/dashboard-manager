0f2fd0af36482202de5d9ca0afdcb76b9c0f946a
Guilherme Barcelo <guilhermebrcneves@gmail.com>
2026-02-09 12:29:12 -0300

    Update dashboard.sql

--- config/dashboard.sql
+++ config/dashboard.sql
@@ -1,38 +1,48 @@
 CREATE DATABASE IF NOT EXISTS enterprise CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS enterpriseRegister (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    senha VARCHAR(100) NOT NULL,
-    telefone VARCHAR(20) NOT NULL
+CREATE TABLE IF NOT EXISTS enterpriseregister (
+  id int(11) NOT NULL,
+  nome varchar(100) NOT NULL,
+  email varchar(100) NOT NULL,
+  senha varchar(100) NOT NULL,
+  telefone varchar(20) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS employeeRegister (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    senha VARCHAR(100) NOT NULL,
-    telefone VARCHAR(20) NOT NULL,
-    enterpriseName VARCHAR(100) NOT NULL
+CREATE TABLE IF NOT EXISTS employeeregister (
+  id int(11) NOT NULL,
+  nome varchar(100) NOT NULL,
+  email varchar(100) NOT NULL,
+  senha varchar(100) NOT NULL,
+  telefone varchar(20) NOT NULL,
+  enterpriseName varchar(100) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS notesRegister (
-    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    vendedor VARCHAR(100) NOT NULL,
-    cliente VARCHAR(100) NOT NULL,
-    produto VARCHAR(100) NOT NULL,
-    price DECIMAL(6, 2) NOT NULL,
-    enterpriseName VARCHAR(100) NOT NULL
-);
+CREATE TABLE IF NOT EXISTS notesregister (
+  id int(11) NOT NULL,
+  vendedor varchar(100) NOT NULL,
+  cliente varchar(100) NOT NULL,
+  produto varchar(100) NOT NULL,
+  price decimal(6,2) NOT NULL,
+  enterpriseName varchar(100) DEFAULT NULL
+)
 
-INSERT INTO notesRegister (vendedor, cliente, produto, price, enterpriseName) VALUES
-('Eduardo', 'Cliente1', 'Produto X', 150.00, 'Teste1'),
-('Fernanda', 'Cliente2', 'Produto Y', 200.50, 'Teste1'),
-('Gabriel', 'Cliente3', 'Produto Z', 300.75, 'Teste1');
+INSERT INTO notesregister (vendedor, cliente, produto, price, enterpriseName) VALUES
+('Funcionário1', 'Cliente1', 'Produto-A', 210.89, 'Dash Enterprise'),
+('Funcionário1', 'Cliente2', 'Produto-B', 123.76, 'Dash Enterprise'),
+('Funcionário1', 'Cliente3', 'Produt-C', 541.90, 'Dash Enterprise'),
+('Funcionário2', 'Cliente4', 'Produto-D', 431.80, 'Dash Enterprise'),
+('Funcionário2', 'Cliente5', 'Produto-E', 43.80, 'Dash Enterprise'),
+('Funcionário2', 'Cliente6', 'Produto-F', 541.90, 'Dash Enterprise');
 
-INSERT INTO employeeRegister (nome, email, senha, telefone, enterpriseName) VALUES 
-('Eduardo', 'eduardo@empresa.com', 'senha123', '1234567890', 'Teste1'),
-('Fernanda', 'fernanda@empresa.com', 'senha456', '0987654321', 'Teste1'),
-('Gabriel', 'gabriel@empresa.com', 'senha789', '1122334455', 'Teste1'),
-('Heloisa', 'heloisa@empresa.com', 'senha000', '5555555555', 'Teste2');
+INSERT INTO enterpriseregister (nome, email, senha, telefone) VALUES
+('Dash Enterprise', 'dash@gmail.com', 123, '1234567890'),
+('DashManager', 'dashmanager@gmail.com', 123, '1234567890');
+
+
+INSERT INTO employeeregister (nome, email, senha, telefone, enterpriseName) VALUES
+('Funcionário1', 'funcionario1@gmail.com', 123, '1234567890', 'Dash Enterprise'),
+('Funcionário2', 'funcionario2@gmail.com', 123, '1234567890', 'Dash Enterprise'),
+('Funcionário4', 'funcionario4@gmail.com', 123, '1234567890', 'DashManager'),
+('Funcionário5', 'funcionario5@gmail.com', 123, '1234567890', 'DashManager'),
+('Funcionário6', 'funcionario6@gmail.com', 123, '1234567890', 'DashManager'),
+('Funcionário8', 'funcionario8@gmail.com', 123, '1234567890', 'Dash Enterprise');
